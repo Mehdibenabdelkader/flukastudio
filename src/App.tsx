2de93aea19450b90637938151159b8fc8ca80592
Fluka <m.benabdelkader02@gmail.com>
2024-06-19 17:19:47 +0100

    chore: Add initial project files and dependencies

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Navbar } from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
