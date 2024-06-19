c2770c3d0008b9faff0e26bd106f4642dfe44a6f
Fluka <m.benabdelkader02@gmail.com>
2024-06-19 17:36:23 +0100

    feat: Add logo to Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,13 @@
+import flukastudio from "../assets/flukastudio.svg";
+
 export const Navbar = () => {
-  return <p>Navbar</p>;
+  return (
+    <div>
+      <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
+        <a className="block w-[12rem] xl:mr-8" href="#hero">
+          <img src={flukastudio} width={190} height={40} alt="Brainwave" />
+        </a>
+      </div>
+    </div>
+  );
 };
