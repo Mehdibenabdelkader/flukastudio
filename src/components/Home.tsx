df0debd3e5c1d84a8a9f7d297652093f8edae222
Fluka <m.benabdelkader02@gmail.com>
2024-06-20 21:24:35 +0100

    chore: Update Home component with new layout and styles

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,63 +1,43 @@
+import slogan from "../assets/slogan.svg";
+import icon from "../assets/icon.svg";
+import insta from "../assets/insta.svg";
+import linkedin from "../assets/linkedin.svg";
+import x from "../assets/x.svg";
+
 export const Home = () => {
   return (
-    <div className="grid grid-cols-3 gap-4 p-4 min-h-screen">
-      <div className="flex flex-col space-y-4">
-        <div className="flex-1">
-          <svg
-            className="w-full h-24 "
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M5 13l4 4L19 7"
-            />
-          </svg>
+    <div className=" flex gap-4 p-4">
+      <div className="flex flex-col">
+        <div className="flex-1 my-16">
+          <img src={slogan} alt="" />
         </div>
-        <div className="flex-1 text-justify">
+        <div className="flex-1 text-justify my-20">
           <p className="text-lg"> Brand Identity ? </p>
           <p className="text-lg"> Social media Design ? </p>
           <p className="text-lg"> Website Building ? </p>
         </div>
-        <div className="flex-1 justify-start">
-          <button className="px-4 py-2 bg-blue-500 text-white rounded">
-            WE HAVE IT ALL!
-          </button>
-        </div>
+
+        <button className="px-4 py-2 bg-purple text-white rounded w-60 my-20">
+          WE HAVE IT ALL!
+        </button>
       </div>
 
-      <div className="flex items-end">
+      <div className="flex flex-col justify-end my-10">
         <div className="w-full">
-          <svg
-            className="w-full h-24 bg-gray-200"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M12 4v16m8-8H4"
-            />
-          </svg>
+          <img src={icon} alt="" />
         </div>
       </div>
 
-      <div className="flex justify-end items-center">
-        <div className="space-x-2">
-          <button className="px-4 py-2 bg-green-500 text-white rounded">
-            Button 1
-          </button>
-          <button className="px-4 py-2 bg-red-500 text-white rounded">
-            Button 2
-          </button>
-        </div>
+      <div className="flex flex-col justify-around my-72 items-center shrink-0">
+        <button className="px-4 py-2">
+          <img src={x} alt="" />
+        </button>
+        <button className="px-4 py-2">
+          <img src={linkedin} alt="" />
+        </button>
+        <button className="px-4 py-2">
+          <img src={insta} alt="" />
+        </button>
       </div>
     </div>
   );
