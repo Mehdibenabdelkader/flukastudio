9f23faf8b933f88f0042b37c4653aef1ec34b04e
Fluka <m.benabdelkader02@gmail.com>
2024-06-22 00:22:14 +0100

    chore: Update Home component layout and styles

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,29 +6,23 @@ import x from "../assets/x.svg";
 
 export const Home = () => {
   return (
-    <div className=" flex gap-4 p-4">
-      <div className="flex flex-col">
-        <div className="flex-1 my-16">
+    <div className="flex gap-4 h-5/6 ">
+      <div className="flex fit flex-col w-full justify-evenly ">
+        <div className="flex-1 my-16 ">
           <img src={slogan} alt="" />
         </div>
-        <div className="flex-1 text-justify my-20">
+        <div className="flex-1 text-justify  ">
           <p className="text-lg"> Brand Identity ? </p>
           <p className="text-lg"> Social media Design ? </p>
           <p className="text-lg"> Website Building ? </p>
         </div>
 
-        <button className="px-4 py-2 bg-purple text-white rounded w-60 my-20">
+        <button className="px-4 py-2 bg-purple text-white rounded w-60 my-8 ">
           WE HAVE IT ALL!
         </button>
       </div>
 
-      <div className="flex flex-col justify-end my-10">
-        <div className="w-full">
-          <img src={icon} alt="" />
-        </div>
-      </div>
-
-      <div className="flex flex-col justify-around my-72 items-center shrink-0">
+      <div className="flex flex-col items-center justify-center shrink-0 ">
         <button className="px-4 py-2">
           <img src={x} alt="" />
         </button>
