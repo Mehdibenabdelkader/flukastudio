a0bf3e6dbdb7e0f8570b6752854b67ecbb3d5c15
fluka <m.benabdelkader02@gmail.com>
2024-06-25 03:03:40 +0100

    chore: Update Hero component layout and styles to be responsive

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,12 +3,12 @@ import slogan from "../assets/slogan.svg";
 
 export const Hero = () => {
   return (
-    <div className="flex gap-4 pt-[12rem] -mt-[5.25rem] h-5/6">
-      <div className="flex fit flex-col w-full justify-evenly bg-slate-400 ">
-        <div className="flex-1 my-16 ">
+    <div className="flex gap-4 mx-auto pt-[12rem] -mt-[5.25rem] w-5/6 h-5/6">
+      <div className="flex fit flex-col w-full justify-evenly  ">
+        <div className="flex-1 my-16">
           <img src={slogan} alt="" />
         </div>
-        <div className="flex-1 text-justify  ">
+        <div className="flex-1 text-justify w-5/6 ">
           <p className="text-lg"> Brand Identity ? </p>
           <p className="text-lg"> Social media Design ? </p>
           <p className="text-lg"> Website Building ? </p>
