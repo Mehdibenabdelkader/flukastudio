2ba047620b71f599687ef9e5c206288cf3746f9d
fluka <m.benabdelkader02@gmail.com>
2024-06-26 16:21:46 +0100

    added project images

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,19 +1,20 @@
+import Maktooba from "../assets/projects/maktooba.png";
 
 export const Projects = () => {
   return (
-    <div className="w-5/6 mx-auto flex flex-col pt-20 ">
+    <div className="w-5/6 mx-auto flex flex-col py-20 ">
       <h1 className="font-black text-6xl pb-10 pt-4">We take Pride in our work</h1>
       <div className="flex gap-9 justify-evenly">
-        <button className="bg-purple">
+        <button className="bg-purple flex-1">
+          <img src={Maktooba} alt="" />
+        </button>
+        <button className="bg-purple flex-1">
           test
         </button>
-        <button className="bg-purple">
+        <button className="bg-purple flex-1">
           test
         </button>
-        <button className="bg-purple">
-          test
-        </button>
-        <button className="bg-purple">
+        <button className="bg-purple flex-1">
           test
         </button>
       </div>
